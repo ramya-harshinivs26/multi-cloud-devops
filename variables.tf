517585797dddce92ff046476ef460cb9f6c45688
Ramya Harshini V S <ramyaaharshinivs8357@gmail.com>
2026-03-03 16:21:20 +0530

    Add variables for VM admin username and password

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "admin_username" {
+  description = "VM admin username"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "VM admin password"
+  type        = string
+  sensitive   = true
+}
